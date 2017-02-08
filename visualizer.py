e5086705a6f35b13c630644fd56b5a1cf0ff72f6
anshuman73 <dev.anshuman73@gmail.com>
2017-02-08 13:07:32 +0530

    Proper sorting for subjects without marks data

--- visualizer.py
+++ visualizer.py
@@ -48,7 +48,7 @@ def excelify():
         sub_worksheet.write(0, 5, 'Grade')
         db_cursor.execute('SELECT sub.Roll_Number, stud.Name, sub.Theory_Marks, sub.Practical_Marks, sub.Total_Marks,'
                           'sub.Grade FROM _{} sub JOIN Students stud ON sub.Roll_Number = stud.Roll_Number '
-                          'ORDER BY sub.Total_Marks DESC, stud.Name ASC'.format(code))
+                          'ORDER BY sub.Total_Marks DESC, sub.Grade ASC, stud.Name ASC'.format(code))
         students_details = db_cursor.fetchall()
         number_of_students = len(students_details)
         for row_num in range(2, number_of_students + 2):
