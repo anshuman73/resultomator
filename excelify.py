d337d1c5575bd24b45ca207f2e9f3ed5b3895479
anshuman73 <dev.anshuman73@gmail.com>
2017-05-17 23:50:19 +0530

    perfect main worksheet analysis and statistics

--- excelify.py
+++ excelify.py
@@ -35,6 +35,7 @@ def excelify():
     main_heading_format = main_workbook.add_format({'bold': True, 'align': 'center', 'valign': 'vcenter',
                                                     'font_size': 13})
     main_left_align_format = main_workbook.add_format({'align': 'left', 'valign': 'vcenter'})
+    main_center_align_format = main_workbook.add_format({'align': 'center', 'valign': 'vcenter'})
     sub_heading_format = subject_workbook.add_format({'bold': True, 'align': 'center', 'valign': 'vcenter',
                                                       'font_size': 13})
     sub_left_align_format = subject_workbook.add_format({'align': 'left', 'valign': 'vcenter'})
@@ -63,7 +64,8 @@ def excelify():
     main_worksheet.write(0, 4, 'Final Result')
 
     subject_data = dict()
-    subject_index = None  # Helps avoid warning and also error detection (Highly improbable)
+    list_of_number_of_subject_students = list()
+
     for subject, code in all_subjects:
         subject_index = all_subjects.index((subject, code))
         sub_worksheet = subject_workbook.add_worksheet(str(subject))
@@ -91,6 +93,7 @@ def excelify():
                           'ORDER BY sub.Grade ASC, sub.Total_Marks DESC, stud.Name ASC'.format(code))
         students_details = [list(result) for result in db_cursor.fetchall()]
         number_of_subject_students = len(students_details)
+        list_of_number_of_subject_students.append(number_of_subject_students)
         for row_num in range(number_of_subject_students):
             students_details[row_num][4] = clean_marks(students_details[row_num][4])  # Required to remove fail chars
             sub_worksheet.set_row(row_num + 2, 18)
@@ -109,7 +112,7 @@ def excelify():
         sub_worksheet.merge_range(row_num + 5, 0, row_num + 5, 5, 'Statistics', sub_heading_format)
         stats_row_num = row_num + 6
         sub_marks_range = xl_range(2, 4, row_num + 2, 4)
-        for x in range(25):
+        for x in range(24):
             sub_worksheet.set_row(stats_row_num + x, 18)
             for y in range(0, 4, 3):
                 sub_worksheet.merge_range(stats_row_num + x, y, stats_row_num + x, y + 2, '')
@@ -151,8 +154,9 @@ def excelify():
         sub_worksheet.write(stats_row_num + 1, 3, '=COUNTIF({}, "<33")'
                             .format(sub_marks_range), sub_center_align_format)
 
-    student_total_index = subject_index * 2 + 9
-    main_worksheet.write(0, student_total_index, 'Student Average')
+    student_average_index = len(all_subjects) * 2 + 7
+    main_worksheet.write(0, student_average_index, 'Student Average')
+    student_average_range = xl_range(3, student_average_index, number_of_students + 2, student_average_index)
 
     for student_index in range(number_of_students):
         main_worksheet.set_row(student_index + 3, 18, main_left_align_format)
@@ -172,9 +176,83 @@ def excelify():
             main_worksheet.write(student_index + 3,
                                  all_subjects.index((subject_data[subject_code]['subject_name'], subject_code)) * 2 + 7,
                                  grade_data)
-        main_worksheet.write(student_index + 3, student_total_index, get_best(english_marks, student_marks))
+        main_worksheet.write(student_index + 3, student_average_index, get_best(english_marks, student_marks))
     main_worksheet.set_row(number_of_students + 5, 30)
     main_worksheet.merge_range(number_of_students + 5, 0, number_of_students + 5, 4, 'Statistics', main_heading_format)
+
+    main_stats_row_num = number_of_students + 6
+    for x in range(24):
+        main_worksheet.set_row(main_stats_row_num + x, 18)
+        main_worksheet.merge_range(main_stats_row_num + x, 0, main_stats_row_num + x, 3, '')
+        for y in range(0, number_of_subjects):
+            main_worksheet.merge_range(main_stats_row_num + x, y * 2 + 6, main_stats_row_num + x, y * 2 + 7, '')
+    main_stats_row_num += 1
+    main_worksheet.write(main_stats_row_num, 0, 'Total Number of students appeared', main_left_align_format)
+    main_worksheet.write(main_stats_row_num + 1, 0, 'Maximum Marks achieved', main_left_align_format)
+    main_worksheet.write(main_stats_row_num + 2, 0, 'Minimum Marks achieved', main_left_align_format)
+    main_worksheet.write(main_stats_row_num + 3, 0, 'Average marks', main_left_align_format)
+    main_stats_row_num += 5
+    for possible_grade in possible_grades:
+        main_worksheet.write(main_stats_row_num, 0, 'Number of {} s'.format(possible_grade), main_left_align_format)
+        main_stats_row_num += 1
+    main_worksheet.write(main_stats_row_num, 0, 'Number of students obtaining >=75 marks', main_left_align_format)
+    main_worksheet.write(main_stats_row_num + 1, 0, 'Number of students obtaining >=90 marks', main_left_align_format)
+    main_stats_row_num += 2
+    for marks in range(80, 30, -10):
+        main_worksheet.write(main_stats_row_num, 0, 'Number of students obtaining marks in the range {} to {}'
+                             .format(marks, marks + 10), main_left_align_format)
+        main_stats_row_num += 1
+    main_worksheet.write(main_stats_row_num, 0, 'Number of students obtaining marks in the range 33 to 40',
+                         main_left_align_format)
+    main_worksheet.write(main_stats_row_num + 1, 0, 'Number of students obtaining marks less than 33',
+                         main_left_align_format)
+    for subject_index, number_of_subject_students in enumerate(list_of_number_of_subject_students):
+        main_stats_data_row_num = main_stats_row_num - 21
+        main_marks_range = xl_range(3, subject_index * 2 + 6, number_of_students + 2, subject_index * 2 + 6)
+        main_worksheet.write(main_stats_data_row_num, subject_index * 2 + 6, number_of_subject_students,
+                             main_center_align_format)
+        main_worksheet.write(main_stats_data_row_num + 1, subject_index * 2 + 6, '=MAX({})'.format(main_marks_range),
+                             main_center_align_format)
+        main_worksheet.write(main_stats_data_row_num + 2, subject_index * 2 + 6, '=MIN({})'.format(main_marks_range),
+                             main_center_align_format)
+        main_worksheet.write(main_stats_data_row_num + 3, subject_index * 2 + 6, '=AVERAGE({})'
+                             .format(main_marks_range), main_center_align_format)
+        main_stats_data_row_num += 5
+        for possible_grade in possible_grades:
+            main_worksheet.write(main_stats_data_row_num, subject_index * 2 + 6, '=COUNTIF({}, "{}")'
+                                 .format(xl_range(3, subject_index * 2 + 7, number_of_students + 2, subject_index * 2 +
+                                                  7), possible_grade), main_center_align_format)
+            main_stats_data_row_num += 1
+        main_worksheet.write(main_stats_data_row_num, subject_index * 2 + 6, '=COUNTIF({}, ">=75")'
+                             .format(main_marks_range), main_center_align_format)
+        
+        main_worksheet.write(main_stats_data_row_num + 1, subject_index * 2 + 6, '=COUNTIF({}, ">=90")'
+                             .format(main_marks_range), main_center_align_format)
+        main_stats_data_row_num += 2
+        for marks in range(80, 30, -10):
+            main_worksheet.write(main_stats_data_row_num, subject_index * 2 + 6, '=COUNTIFS({}, ">={}", {}, "<{}")'
+                                 .format(main_marks_range, marks, main_marks_range, marks + 10),
+                                 main_center_align_format)
+            main_stats_data_row_num += 1
+        
+        main_worksheet.write(main_stats_data_row_num, subject_index * 2 + 6, '=COUNTIFS({}, ">=33", {}, "<40")'
+                             .format(main_marks_range, main_marks_range), main_center_align_format)
+        
+        main_worksheet.write(main_stats_data_row_num + 1, subject_index * 2 + 6, '=COUNTIF({}, "<33")'
+                             .format(main_marks_range), main_center_align_format)
+
+    final_stats_row_num = main_stats_row_num - 21
+    main_worksheet.write(final_stats_row_num, student_average_index - 1, 'Number of students', main_heading_format)
+    main_worksheet.write(final_stats_row_num, student_average_index, number_of_students, main_heading_format)
+    main_worksheet.write(final_stats_row_num + 1, student_average_index - 1, 'Highest Average', main_heading_format)
+    main_worksheet.write(final_stats_row_num + 1, student_average_index, '=MAX({})'.format(student_average_range),
+                         main_heading_format)
+    main_worksheet.write(final_stats_row_num + 2, student_average_index - 1, 'Lowest Average', main_heading_format)
+    main_worksheet.write(final_stats_row_num + 2, student_average_index, '=MIN({})'.format(student_average_range),
+                         main_heading_format)
+    main_worksheet.write(final_stats_row_num + 3, student_average_index - 1, 'School Average', main_heading_format)
+    main_worksheet.write(final_stats_row_num + 3, student_average_index, '=AVERAGE({})'.format(student_average_range),
+                         main_heading_format)
 
     main_workbook.close()
     subject_workbook.close()
