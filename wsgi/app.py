16d1186a81191a980b9d0b881193ae95af50c6fa
anshuman73 <dev.anshuman73@gmail.com>
2017-05-28 01:06:42 +0530

    Commit to make it work

--- wsgi/app.py
+++ wsgi/app.py
@@ -1,11 +1,21 @@
-from wsgi import network_processor
-from wsgi import file_processor
-from wsgi import data_cleaner
-from wsgi import excelify
+import network_processor
+import file_processor
+import data_cleaner
+import excelify
+import os
+from flask import Flask, request, redirect, url_for, render_template, flash
+from werkzeug.utils import secure_filename
 
-from flask import Flask, render_template, request
+UPLOAD_FOLDER = os.environ['OPENSHIFT_DATA_DIR'] + '/txt_files'
+ALLOWED_EXTENSIONS = ['txt']
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/')
@@ -16,11 +26,28 @@ def form():
 @app.route('/result', methods=['POST'])
 def result():
     if request.method == 'POST':
-        results = request.form.to_dict()
-        file_processor.process(file_address)
-        return render_template("results.html", results=results)
+        # results = request.form.to_dict()
+        # file_processor.process(file_address)
+        # return render_template("results.html", results=results)
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
     else:
         return 'NOT ALLOWED !'
+
+app.config['PROPAGATE_EXCEPTIONS'] = True
 
 if __name__ == '__main__':
     app.run(debug=True)  # TODO: Remember to shut this off
